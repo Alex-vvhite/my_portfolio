c3362cac70707c9609ef89739366e35fff423a48
AlexVVhite <xodyrev.aleksej42@gmail.com>
2025-02-06 22:49:38 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,33 @@
-
 import './Footer.css';
+import tg_social from './links/tg.svg';
+import gh_social from './links/gh.svg';
 
 function Footer() {
 
  
   return (
     <footer className="footer">
-         Провера
+        <div className="container">
+            <div className="footer__element">
+                <div className="fooer__elemet-social">
+                    <ul className="links_social">
+                        <li className="links_social-elem">
+                            <a href="https://t.me/Alex_frilance" class="elem__link">
+                                <img className="image_social" src={tg_social} alt=""/>
+                            </a>
+                        </li>
+                        <li className="links_social-elem">
+                            <a href="https://github.com/Alex-vvhite?tab=repositories" className="elem__link">
+                                <img className="image_social" src={gh_social} alt=""/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="footer__elemet-about">
+                    Собрано и создано собственно-ручками и собственно-ножками
+                </div>
+            </div>
+        </div>
     </footer>
   );
   
